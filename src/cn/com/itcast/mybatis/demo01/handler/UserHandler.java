e2cc8aeb02b8f6faf1d58d0508b8ba6227c45764
reaperbug <269022448@qq.com>
2018-03-29 23:46:57 +0800

    add findUserByName method.

--- src/cn/com/itcast/mybatis/demo01/handler/UserHandler.java
+++ src/cn/com/itcast/mybatis/demo01/handler/UserHandler.java
@@ -9,21 +9,31 @@ import org.junit.Test;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class UserHandler {
+    //mybatis配置文件
+    private String resource = "SqlMapConfig.xml";
+    //得到配置文件流
+    InputStream inputStream;
+
+    {
+        try {
+            inputStream = Resources.getResourceAsStream(resource);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    //创建会话工厂，传入mybatis的配置文件信息
+    SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
     /**
      * call
      */
     @Test
     public void findUserById() throws IOException {
-
-        //mybatis配置文件
-        String resource = "SqlMapConfig.xml";
-        //得到配置文件流
-        InputStream inputStream = Resources.getResourceAsStream(resource);
-        //创建会话工厂，传入mybatis的配置文件信息
-        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
-
         //通过会话工厂得到SqlSession
         SqlSession sqlSession = sqlSessionFactory.openSession();
 
@@ -37,6 +47,21 @@ public class UserHandler {
         }
         //通过SqlSession操作数据库
 
-
+    }
+    /**
+     * 根据用户名称模糊查询
+     */
+    @Test
+    public void findUserByName(){
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        List<User> userList = null;
+        try{
+            userList = sqlSession.selectList("test.findUserByName","张");
+        }catch(Exception e){
+            e.printStackTrace();
+        }finally {
+            sqlSession.close();
+            System.out.println(userList);
+        }
     }
 }
